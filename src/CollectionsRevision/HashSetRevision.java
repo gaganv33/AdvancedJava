601bd19a0a1604018b83184a6ae7b838cfe18f5a
Gagan V <191381956gaganctvs@gmail.com>
2024-09-28 23:23:48 +0530

    HashSet comments

--- src/CollectionsRevision/HashSetRevision.java
+++ src/CollectionsRevision/HashSetRevision.java
@@ -9,6 +9,7 @@ public class HashSetRevision {
         List<Name> v = new ArrayList<>(List.of(new Name("a", "v"), new Name("t", "q"), new Name("b", "p"), new Name("a", "v")));
         List<Name> w = new ArrayList<>(List.of(new Name("v", "q"), new Name("r", "u"), new Name("b", "p"), new Name("q", "vz")));
 
+        // s.add(Object): Used to add the specified element if it is not present, if it is present then return false.
         for(var element: v) {
             boolean res = s.add(element);
             if(res) {
@@ -17,15 +18,23 @@ public class HashSetRevision {
                 System.out.println("Element is already present: " + element);
             }
         }
+
+        // s.contains(Object): Used to return true if an element is present in a set.
         if(s.contains(new Name("a", "v"))) {
             System.out.println("Element found.");
         } else {
             System.out.println("Element not found");
         }
+
+        // s.remove(Object): Used to remove the element if it is present in set.
         s.remove(new Name("a", "v"));
         print(s);
+
+        // s.addAll(Collection): This method is used to append all the elements from the mentioned collection to the existing set.
         s.addAll(w);
         print(s);
+
+        // s.containsAll(Collection): This method is used to check whether the set contains all the elements present in the given collection or not.
         System.out.println("containsAll: " + s.containsAll(w));
 
         System.out.println("forEach method: ");
@@ -33,12 +42,18 @@ public class HashSetRevision {
             System.out.println(element.firstName + " " + element.secondName);
         });
 
+        // s.retainAll(Collection): This method is used to retain all the elements from the set which are mentioned in the given collection.
+        // This method returns true if this set changed as a result of the call.
         s.retainAll(w);
         print(s);
+
+        // s.removeIf(Predicate): Removes all the elements of this collection that satisfy the given predicate.
         s.removeIf((element) -> {
             return element.firstName.equalsIgnoreCase("q");
         });
         print(s);
+
+        // s.removeAll(Collection): Removes from this set all of its elements that are contained in the specified collection (optional operation).
         s.removeAll(w);
         print(s);
     }
