0f9c2cee1ebfa8008b0dc73a086aa08260c8bb50
Gagan V <191381956gaganctvs@gmail.com>
2024-09-29 10:36:49 +0530

    LinkedHashSet comments

--- src/CollectionsRevision/LinkedHashSetRevision.java
+++ src/CollectionsRevision/LinkedHashSetRevision.java
@@ -6,11 +6,14 @@ public class LinkedHashSetRevision {
     record Name(String firstName, String secondName) {}
     public static void start() {
         Set<Name> s = new LinkedHashSet<>();
-        List<Name> v = new ArrayList<>(List.of(new Name("a", "r"), new Name("r", "qw"), new Name("p", "qw"), new Name("a", "r")));
-        List<Name> w = new ArrayList<>(List.of(new Name("ad", "r"), new Name("rr", "w"), new Name("y", "qzw"), new Name("at", "rq")));
+        List<Name> v = new ArrayList<>(List.of(new Name("a", "r"), new Name("r", "qw"), new Name("p", "qw"),
+                new Name("a", "r")));
+        List<Name> w = new ArrayList<>(List.of(new Name("ad", "r"), new Name("rr", "w"), new Name("y", "qzw"),
+                new Name("at", "rq")));
         Name a = new Name("rr", "q");
         Name b = new Name("rr", "w");
 
+        // s.add(Object): Adds the specified element to this set if it is not already present.
         for(var element : v) {
             boolean res = s.add(element);
             if(res) {
@@ -19,12 +22,21 @@ public class LinkedHashSetRevision {
                 System.out.println("Element already present: " + element);
             }
         }
+
+        // s.addAll(Collection): Adds all of the elements in the specified collection to this collection
+        // (optional operation).
         s.addAll(w);
         print(s);
+
+        // s.contains(Object): Returns true if this set contains the specified element.
         System.out.println("contains: " + s.contains(a) + " " + a);
         System.out.println("contains: " + s.contains(b) + " " + b);
+
+        // s.containsAll(Collection): Returns true if this collection contains all the elements in the specified
+        // collection.
         System.out.println("containsAll: " + s.containsAll(w));
 
+        // s.remove(Object): Removes the specified element from this set if it is present.
         System.out.println("remove: " + a);
         s.remove(a);
         print(s);
@@ -33,6 +45,7 @@ public class LinkedHashSetRevision {
         s.remove(b);
         print(s);
 
+        // s.removeIf(Predicate): Removes all the elements of this collection that satisfy the given predicate.
         s.removeIf((element) -> {
             return element.firstName.toLowerCase().charAt(0) == 'a';
         });
@@ -43,10 +56,14 @@ public class LinkedHashSetRevision {
             System.out.println(element.firstName + " " + element.secondName);
         });
 
+        // s.retainAll(Collection): This method is used to retain all the elements from the set which are mentioned
+        // in the given collection. This method returns true if this set changed as a result of the call.
         System.out.println("retainAll method:");
         s.retainAll(w);
         print(s);
 
+        // s.removeAll(Collection): This method is used to remove all the elements from the collection which are
+        // present in the set. This method returns true if this set changed as a result of the call.
         System.out.println("removeAll method:");
         s.removeAll(w);
         print(s);
